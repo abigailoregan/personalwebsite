7d6b5b9c70c10fc664f7e27c7bbc26bf0a2ef207
David O'Regan <oreagn10@gmail.com>
2024-08-06 17:55:00 -0400

    Carousel Update #2
    - added new art
    - prepared more hiRez images
    - photo re-orders

--- javascript/popups.js
+++ javascript/popups.js
@@ -1,46 +1,145 @@
-const abstract_art = { // Needs updating
+const abstract_art = {
     name: 'abstract',
-    paths: [ '../hiRez/abstract/abigail_time.jpg', '../hiRez/abstract/abigail_pigeon.jpg', '../hiRez/abstract/abigail_shapes.jpg', '../hiRez/abstract/abigail_protovine.jpg', '../hiRez/abstract/abigail_vines.jpg' ],
-    descs: [ 'Time, Collage on Board, 20"x24"', 'Celestial Oddities, Collage on Paper, 14"x10"', 'Shapes, Micron Pens on Watercolor Paper, 30"x22"', 'Proto-Vines, Micron Pens on Watercolor Paper - Study for Overgrowth', 'Overgrowth, Micron Pens on Watercolor Paper, 30"x22"' ],
-    final: 4
-}
-
-const landscapes_art = { // Needs updating
-    name: 'landscapes',
-    paths: [ '../hiRez/landscapes/abigail_fireycreek.jpg', '../hiRez/landscapes/abigail_suffocation.jpg', '../hiRez/landscapes/abigail_fallschurch.jpg', '../hiRez/landscapes/abigail_birds.jpg', '../hiRez/landscapes/abigail_tree.jpg', '../hiRez/landscapes/abigail_richmond.jpg'],
-    descs: [ 'Fiery Creek, Chalk Pastel on Pastel Paper, 12"x16"', 'Suffocation, Mixed Media on Canvas Board, 20"x24"', 'The Little City, Oil Paint on Canvas, 12"x16"', 'David\'s Birds, Oil Paint on Canvas, 20"x30"', 'Twilight\'s Guardian, Gouache on Hardcover Book', 'Richmond, Acrylic Paint on Canvas, 20"x16"' ],
+    paths: [
+        '../hiRez/abstract/abigail_time.jpg',
+        '../hiRez/abstract/abigail_pigeon.jpg',
+        '../hiRez/abstract/abigail_inverse.jpg',
+        '../hiRez/abstract/abigail_shapes.jpg',
+        '../hiRez/abstract/abigail_protovine.jpg',
+        '../hiRez/abstract/abigail_vines.jpg'
+    ],
+    descs: [
+        'Time, Collage on Board, 20"x24"',
+        'Celestial Oddities, Collage on Paper, 14"x10"',
+        'Proto-Shapes, Micron Pens on Watercolor Paper - Study for Overgrowth',
+        'Shattered Harmony, Micron Pens on Watercolor Paper, 30"x22"',
+        'Proto-Vines, Micron Pens on Watercolor Paper - Study for Overgrowth',
+        'Overgrowth, Micron Pens on Watercolor Paper, 30"x22"'
+    ],
     final: 5
 }
 
-const love_art = { // Complete Set
+const figures_art = {
+    name: 'figures',
+    paths: [
+        '../hiRez/figures/abigail_siren.jpg',
+        '../hiRez/figures/abigail_dancer.jpg',
+        '../hiRez/figures/abigail_lady.jpg',
+        '../hiRez/figures/abigail_dude2.jpg',
+        '../hiRez/figures/abigail_figures.jpg',
+        '../hiRez/figures/abigail_oldman.jpg'
+    ],
+    descs: [
+        'Title, Medium, Size',
+        'The Flamenco Dancer, digital drawing on Procreate',
+        'Title, Medium, Size',
+        'Title, Medium, Size',
+        'Studies of the female form, graphite on newsprint',
+        'Title, Medium, Size'
+    ],
+    final: 5
+}
+
+const landscapes_art = {
+    name: 'landscapes',
+    paths: [
+        '../hiRez/landscapes/abigail_forest.jpg',
+        '../hiRez/landscapes/abigail_fireycreek.jpg',
+        '../hiRez/landscapes/abigail_forest2.jpg',
+        '../hiRez/landscapes/abigail_suffocation.jpg',
+        '../hiRez/landscapes/abigail_fallschurch.jpg',
+        '../hiRez/landscapes/abigail_birds.jpg',
+        '../hiRez/landscapes/abigail_tree.jpg',
+        '../hiRez/landscapes/abigail_richmond.jpg'
+    ],
+    descs: [
+        'Winter, Acrylic Paint on Canvas Board, 14"x11"',
+        'Fiery Creek, Chalk Pastel on Pastel Paper, 12"x16"',
+        'Tranquility, Oil Paint on Canvas Board, 8"x10" - Art Study with Johannes Vloothuis',
+        'Suffocation, Mixed Media on Canvas Board, 20"x24"',
+        'The Little City, Oil Paint on Canvas, 12"x16"',
+        'David\'s Birds, Oil Paint on Canvas, 20"x30"',
+        'Twilight\'s Guardian, Gouache on Hardcover Book',
+        'Richmond, Acrylic Paint on Canvas, 20"x16"'
+    ],
+    final: 7
+}
+
+const love_art = {
     name: 'love',
-    paths: [ '../hiRez/love/abigail_summon.jpg', '../hiRez/love/abigail_noah.jpg', '../hiRez/love/abigail_noahsana.jpg' ],
-    descs: [ 'Flickering Fate, Charcoal on Paper, 18"x24"', 'The Veil, Charcoal on Paper, 24"x18"', 'Dancing with Death, Charcoal on Paper, 44"x30"' ],
+    paths: [
+        '../hiRez/love/abigail_summon.jpg',
+        '../hiRez/love/abigail_noah.jpg',
+        '../hiRez/love/abigail_noahsana.jpg'
+    ],
+    descs: [
+        'Flickering Fate, Charcoal on Paper, 18"x24"',
+        'The Veil, Charcoal on Paper, 24"x18"',
+        'Dancing with Death, Charcoal on Paper, 44"x30"'
+    ],
     final: 2
 }
 
-const murals_art = { // Up to Date
+const murals_art = {
     name: 'murals',
-    paths: [ '../hiRez/murals/abigail_drain.jpg', '../hiRez/murals/abigail_goddess.jpg' ],
-    descs: [ 'Storm Drain Mural 1, <a class="link-button" href="https://sites.google.com/view/falls-church-art-walk/home-artwork-inventory/storm-drain-mural-1?authuser=0" target="_blank">learn more</a>', 'Goddess of Justice, Acrylic Mural, Meridian High School - Falls Church, VA' ],
+    paths: [
+        '../hiRez/murals/abigail_drain.jpg',
+        '../hiRez/murals/abigail_goddess.jpg'
+    ],
+    descs: [
+        'Storm Drain Mural 1, <a class="link-button" href="https://sites.google.com/view/falls-church-art-walk/home-artwork-inventory/storm-drain-mural-1?authuser=0" target="_blank">click here to learn more</a>',
+        'Goddess of Justice, Acrylic Mural, Meridian High School - Falls Church, VA'
+    ],
     final: 1
 }
 
-const people_art = { // Needs starting
-    name: 'people',
-    paths: [ '' ],
-    descs: [ '' ],
-    final: 0
+const portraits_art = {
+    name: 'portraits',
+    paths: [
+        '../hiRez/portraits/abigail_blue.jpg',
+        '../hiRez/portraits/abigail_hera.jpg',
+        '../hiRez/portraits/abigail_julia.jpg',
+        '../hiRez/portraits/abigail_digital.jpg',
+        '../hiRez/portraits/abigail_self1.jpg',
+        '../hiRez/portraits/abigail_eye.jpg',
+        '../hiRez/portraits/abigail_einstein.jpg',
+        '../hiRez/portraits/abigail_phone.jpg',
+        '../hiRez/portraits/abigail_metal.jpg',
+        '../hiRez/portraits/abigail_beardold.jpg',
+        '../hiRez/portraits/abigail_holland.jpg',
+        '../hiRez/portraits/abigail_self2.jpg',
+        '../hiRez/portraits/abigail_droregan.jpg'
+    ],
+    descs: [
+        'Female Portrait Study, Gouache on Mixed Media Paper',
+        'Hera, Micron Pen and White Charcoal on Toned-Tan Mixed Media Paper',
+        'Julia, Charcoal on Paper, 24"x18"',
+        'Color Study, digital drawing on Procreate',
+        'Abigail O\'Regan Graffiti, Mixed Media Painting on Canvas, 20"x30"',
+        'The Eye, Acrylic on Canvas Board, 4"x6"',
+        'Einstein, Micron Pen and White Charcoal on Toned-Tan Mixed Media Paper',
+        'Man on Phone, Charcoal on Drawing Paper',
+        'Hope, Acrylic on Canvas, 18"x14"',
+        'Old Man, Charcoal on Drawing Paper',
+        'Tom Holland - Uncharted,  Oil on Canvas Board, 8"x10"',
+        'Abigail O\'Regan, Acrylic Paint on Wood Panel, 40"x30"',
+        'Dr David J O\'Regan, Oil on Canvas, 18"x14"'
+    ],
+    final: 12
 }
 
-const stills_art = { // Needs starting
+const stills_art = {
     name: 'stills',
-    paths: [ '' ],
-    descs: [ '' ],
+    paths: [
+        ''
+    ],
+    descs: [
+        'Title, Medium, Size'
+    ],
     final: 0
 }
 
-const setList = [ abstract_art, landscapes_art, love_art, murals_art, people_art, stills_art ]
+const setList = [ abstract_art, figures_art, landscapes_art, love_art, murals_art, portraits_art, stills_art ]
 
 function closePopUp(node) {
     document.body.classList.remove('disabled')
